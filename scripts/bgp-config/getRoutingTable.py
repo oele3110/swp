8dabc6e2d2f4ccdb4843622b035f928dee924c3c
tanger <oele3110@hotmail.com>
2014-01-20 20:38:48 +0100

    getRouting table changed, but still bugs

--- scripts/bgp-config/getRoutingTable.py
+++ scripts/bgp-config/getRoutingTable.py
@@ -1,33 +1,55 @@
 #!/usr/bin/env python
 
-import subprocess
 import re
+import sys
+import getpass
+import telnetlib
+
+HOST = "localhost"
+PORT = 2000
 
 def main():
 	
-	command = "/home/skims/swp/scripts/bgp-config/getRoutingTable.sh"
-	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
-	
-	output = process.communicate()
-	
-	lines = str(output[0]).split("\n")
-	
-	regex = "(K|C|S|R|O|I|B|A)\>\*\s(.*)"
-	
-	
-	for line in lines:
-		result = re.match(regex, line)
-		if result:
-			print line
-		if "Codes: K - kernel route, C - connected, S - static, R - RIP," in line:
-			print line
-		if "O - OSPF, I - IS-IS, B - BGP, A - Babel," in line:
-			print line
-		if "> - selected route, * - FIB route" in line:
-			print line + "\n"
-	
-	
-
+	if len(sys.argv) < 2:
+		print("error, parameter missing")
+	else:
+		if sys.argv[1] == "-g":
+			
+			tn = telnetlib.Telnet(HOST, PORT)
+			
+			tn.read_until("Zebra>")
+			
+			tn.write("show ip route\n")
+			tn.write("exit\n")
+			
+			output = tn.read_all()
+			
+			lines = output.split("\n")
+			
+			regex = "(K|C|S|R|O|I|B|A)\>\*\s(.*)"
+			
+			resultStr = ""
+			
+			for line in lines:
+					result = re.match(regex, line)
+					if result:
+							#print line
+							resultStr += line + "\n"
+					"""
+					if "Codes: K - kernel route, C - connected, S - static, R - RIP," in line:
+							#print line
+							resultStr += line + "\\n"
+					if "O - OSPF, I - IS-IS, B - BGP, A - Babel," in line:
+							#print line
+							resultStr += line + "\n"
+					if "> - selected route, * - FIB route" in line:
+							#print line + "\n"
+							resultStr += line + "\\n\\n"
+					"""
+			
+			print sys.argv[2]
+			print "string"
+			print resultStr
 
 
 # start main method
