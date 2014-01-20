c0e6b27595f63ce14d7d413bc1bf0bbec56e84a2
tanger <oele3110@hotmail.com>
2014-01-20 20:25:04 +0100

    getPrefixes works now

--- scripts/bgp-config/getPrefixes.py
+++ scripts/bgp-config/getPrefixes.py
@@ -1,31 +1,49 @@
 #!/usr/bin/env python
 
-import subprocess
 import re
+import sys
+import getpass
+import telnetlib
+
+HOST = "localhost"
+PORT = 2001
 
 def main():
-	
-	command = "/home/skims/getPrefixes.sh"
-	process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=None, shell=True)
-	
-	output = process.communicate()
-	
-	lines = str(output[0]).split("\n")
-	
-	regex = "\*\>\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+\d+\s+(\d+)\s+.+"
-	
-	prefixes = []
-	
-	for line in lines:
-		#print line
-		result = re.match(regex, line) 
-		if result:
-			prefixes.append(result.group(1))
-	
-	for prefix in prefixes:
-		print prefix
-	
-	
+
+	if len(sys.argv) < 2:
+		print("error, parameter missing")
+	else:
+		if sys.argv[1] == "-g":
+			
+			tn = telnetlib.Telnet(HOST, PORT)
+			
+			tn.read_until("AS65001>")
+			
+			tn.write("show ip bgp\n")
+			tn.write("exit\n")
+			
+			output = tn.read_all()
+			
+			lines = output.split("\n")
+			
+			# this regex filters all announced prefixes
+			regex = "\*\>\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})\s*(0.0.0.0)\s+\d+\s+(\d+)\s+.+"
+			
+			prefixes = []
+			
+			for line in lines:
+				result = re.match(regex, line) 
+				if result:
+					prefixes.append(result.group(1))
+			
+			resultStr = ""
+			
+			for prefix in prefixes:
+				resultStr += prefix + "\\n"
+			
+			print sys.argv[2]
+			print "string"
+			print resultStr
 
 
 
--- scripts/bgp-config/getPrefixes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-(
-echo "show ip bgp"
-sleep 1
-echo -e "exit\r"
-) | telnet localhost 2001
