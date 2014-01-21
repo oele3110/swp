f2e73758587c01937e9276c889baac777d7422f6
tanger <oele3110@hotmail.com>
2014-01-21 15:40:32 +0100

    added zebra to start script

--- scripts/run/start-bgp.sh
+++ scripts/run/start-bgp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/usr/local/sbin/zebra -f /etc/quagga/zebra.conf -i /usr/local/quagga/zebra.pi-p 2000 -P 2000 &
+#bgpd -f /etc/quagga/AS65001.conf -i /usr/local/quagga/AS65001.pid -p 20001 -P 2001 &
 bgpd -f /etc/quagga/AS65002.conf -i /usr/local/quagga/AS65002.pid -p 20002 -P 2002 &
 bgpd -f /etc/quagga/AS65003.conf -i /usr/local/quagga/AS65003.pid -p 20003 -P 2003 &
 
